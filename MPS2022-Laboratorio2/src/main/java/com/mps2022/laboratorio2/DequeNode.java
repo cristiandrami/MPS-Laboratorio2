a49d3e7ad9c2589775fbe5d3576524f3435a5662
Alvaro Aleman <alvaro14uma@gmail.com>
2022-03-15 12:03:09 +0100

    commit
    pom añadido y dequenodetest

--- MPS2022-Laboratorio2/src/main/java/com/mps2022/laboratorio2/DequeNode.java
+++ MPS2022-Laboratorio2/src/main/java/com/mps2022/laboratorio2/DequeNode.java
@@ -30,10 +30,12 @@ public class DequeNode<T> {
     }
 
     public boolean isFirstNode() {
+
         return previous == null ;
     }
 
     public boolean isLastNode() {
+
         return next == null ;
     }
 
