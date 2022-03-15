e1bbaa5daf442d4ba6f574b0191e78f2152e707b
Cristian Dramisino <cristiandrami@libero.it>
2022-03-15 11:59:32 +0100

    DoubleLinkedListQueue class implementation added

---
+++ MPS2022-Laboratorio2/src/main/java/com/mps2022/laboratorio2/DoubleLinkedListQueue.java
@@ -0,0 +1,96 @@
+package com.mps2022.laboratorio2;
+
+public class DoubleLinkedListQueue implements DoubleEndedQueue{
+    private DequeNode head=null;
+    private DequeNode tail=null;
+    private int size=0;
+
+    @Override
+    public void append(DequeNode node) {
+        if(node==null){
+            throw new RuntimeException("node is null, cannot append it");
+        }
+        if(head == null) {
+            head = tail = node;
+            head.setPrevious(null);
+            tail.setNext(null);
+        }
+        else {
+
+            tail.setNext(node);
+            node.setPrevious(tail);
+            tail = node;
+            tail.setNext(null);
+        }
+
+        size++;
+
+
+    }
+
+    @Override
+    public void appendLeft(DequeNode node) {
+
+        if(node==null){
+            throw new RuntimeException("node is null, cannot append it");
+        }
+        if(head == null ) {
+            head =tail= node;
+        }if(head==tail){
+            head.setPrevious(node);
+            node.setNext(head);
+            head = node;
+            tail.setPrevious(head);
+        } else {
+            head.setPrevious(node);
+            node.setNext(head);
+            head = node;
+        }
+
+
+        size++;
+    }
+
+    @Override
+    public void deleteFirst() {
+        if(head==null){ return;}
+        else if(head==tail){
+            head=tail=null;
+        }else{
+            head =(DequeNode) head.getNext();
+            if(this.head != null)
+                head.setPrevious(null);
+
+        }
+        size--;
+    }
+
+    @Override
+    public void deleteLast() {
+        if(tail==null){ return;}
+        else if(head==tail){
+            head=tail=null;
+        }else{
+            tail =(DequeNode) tail.getPrevious();
+            if(tail != null)
+                head.setNext(null);
+
+        }
+        size--;
+    }
+
+    @Override
+    public DequeNode peekFirst() {
+        return head;
+    }
+
+    @Override
+    public DequeNode peekLast() {
+        return tail;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
